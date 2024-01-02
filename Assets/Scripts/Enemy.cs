fe8717a59391d08f6d0c9738a90110395cc660a5
xoghk9zla <xorbs9rla@naver.com>
2024-01-02 19:43:20 +0900

    Update Enemy
    ScareCrow 스프라이트 피벗 변경
    Enemy 이동, 방향전환 추가

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     private Animator animator;
-    private BoxCollider2D boxCollider2D;
+    private BoxCollider2D boxCollider2d;
+    private Rigidbody2D rigid;
 
-    private float maxHp;
-    private float curHp;
+    [SerializeField] LayerMask ground;
+    [SerializeField] private BoxCollider2D trigger;
+    [SerializeField] private bool isGround = false;
+
+    [SerializeField] private float maxHp = 20.0f;
+    [SerializeField] private float curHp;
+    [SerializeField] private float moveSpeed = 0.3f;
 
     public enum enumEnemyType
     {
@@ -20,12 +27,13 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(boxCollider2D != null && collision.gameObject.layer == LayerMask.NameToLayer("Skill")) 
+        if(boxCollider2d != null && collision.gameObject.layer == LayerMask.NameToLayer("Skill")) 
         {
             animator.SetBool("IsHit", true);
         }
@@ -34,7 +42,46 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround();
+        Moving();
+    }
+
+    private void FixedUpdate()
+    {
+        if (trigger.IsTouchingLayers(ground) == false && isGround)
+        {
+            Turn();
+        }
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0.0f, Vector2.down, 0.025f, LayerMask.GetMask("Ground"));
+
+        isGround = false;
+
+        if (hit.transform != null && hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGround = true;
+        }
+
+    }
+
+    private void Moving()
+    {
+        if(isGround && enemyType != enumEnemyType.ForestKeeper && enemyType != enumEnemyType.ScareCrow)
+        {
+            rigid.velocity = new Vector2(moveSpeed, rigid.velocity.y);
+        }
+    }
+
+    private void Turn()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1.0f;
+        moveSpeed *= -1.0f;
+
+        transform.localScale = scale;
     }
 
     public void Hit(float _damage)
