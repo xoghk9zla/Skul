7e44062737f8b93e00ecd8d59b0a0829e9704b67
xoghk9zla <xorbs9rla@naver.com>
2024-01-17 04:42:41 +0900

    Add GrimReaperHead

---
+++ Assets/Scripts/SkulHead.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkulHead : MonoBehaviour
+{
+    [SerializeField] GameObject InteractUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            InteractUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            InteractUI.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
