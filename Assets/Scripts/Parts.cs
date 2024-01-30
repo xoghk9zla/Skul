43982f0a0dd8e7e4452c8838c1164a545056313b
xoghk9zla <xorbs9rla@naver.com>
2024-01-30 19:29:58 +0900

    Update part
    part 생성 시 랜덤 방향으로 힘 받게 변경

--- Assets/Scripts/Parts.cs
+++ Assets/Scripts/Parts.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class Parts : MonoBehaviour
 {
     private float lifeTime = 10.0f;
+    private Rigidbody2D rigid;
+    private Vector2 force;
+    private float rotateforce;
+
+    private void Start()
+    {
+        force.x = Random.Range(-3.0f, 3.0f);
+        force.y = Random.Range(-3.0f, 3.0f);
+        rotateforce = Random.Range(-3.0f, 3.0f);
+
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.AddForce(force, ForceMode2D.Impulse);
+        rigid.AddTorque(rotateforce);
+    }
 
     // Update is called once per frame
     void Update()
