2cab747003792b1ac66e4f1f4fd0ed628b375c25
xoghk9zla <xorbs9rla@naver.com>
2024-02-26 19:54:41 +0900

    Add Item
    아이템 스크립트 추가
    플레이어와 상호작용 할 수 있게 추가
    아이템 획득, 작동 추가 해야함

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] GameObject InteractUI;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            InteractUI.SetActive(true);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if(collision != null && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("È¹µæ");
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
-
-    private void Update()
-    {
-        
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            InteractUI.SetActive(false);
+        }
     }
 }
